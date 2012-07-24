ac734515148b9ed35f6c3c18a3fab3f955429c47
Andrey Kumanyaev <me@zzet.org>
2012-07-25 00:44:46 +0400

    sessions

--- dating/app/controllers/web/sessions_controller.rb
+++ dating/app/controllers/web/sessions_controller.rb
@@ -3,12 +3,33 @@ class Web::SessionsController < Web::ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by_email(params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      sign_in(@user)
+      redirect_to admin_root_path
+    else
+      @user = User.new(params[:user])
+      flash[:alert] = t("authenticate_error")
+      render :action => :new
+    end
+    @user = User.find_by_email(params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      sign_in(@user)
+      redirect_to admin_root_path
+    else
+      @user = User.new(params[:user])
+      flash[:alert] = t("authenticate_error")
+      render :action => :new
+    end
   end
 
   def destroy
+    sign_out
+    redirect_to :root
   end
 
   def update
