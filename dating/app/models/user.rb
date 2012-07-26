8f432290b498b27e584b3d750eac5a965a4e0365
Andrey Kumanyaev <me@zzet.org>
2012-07-26 22:46:56 +0400

    fix locales

--- dating/app/models/user.rb
+++ dating/app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
   include UsefullScopes
 
-  attr_accessible :avatar, :email, :firstname, :lastname, :nickname, :password, :password_digest, :remember_me, :weburl
+  attr_accessible :avatar,
+                  :email,
+                  :firstname,
+                  :lastname,
+                  :nickname,
+                  :password,
+                  :password_digest,
+                  :remember_me,
+                  :weburl
 
   has_secure_password
 end
